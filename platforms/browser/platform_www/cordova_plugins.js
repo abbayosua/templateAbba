9e84ef0352ac57a507034e3f0fa257c95cdbd61a
Abba yosua <abba.yosua@fwd.com>
2019-01-27 22:24:06 +0800

    add plugin camera

--- platforms/browser/platform_www/cordova_plugins.js
+++ platforms/browser/platform_www/cordova_plugins.js
@@ -37,6 +37,36 @@ module.exports = [
         "id": "cordova-plugin-battery-status.Battery",
         "pluginId": "cordova-plugin-battery-status",
         "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-camera/www/CameraConstants.js",
+        "id": "cordova-plugin-camera.Camera",
+        "pluginId": "cordova-plugin-camera",
+        "clobbers": [
+            "Camera"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-camera/www/CameraPopoverOptions.js",
+        "id": "cordova-plugin-camera.CameraPopoverOptions",
+        "pluginId": "cordova-plugin-camera",
+        "clobbers": [
+            "CameraPopoverOptions"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-camera/www/Camera.js",
+        "id": "cordova-plugin-camera.camera",
+        "pluginId": "cordova-plugin-camera",
+        "clobbers": [
+            "navigator.camera"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-camera/src/browser/CameraProxy.js",
+        "id": "cordova-plugin-camera.CameraProxy",
+        "pluginId": "cordova-plugin-camera",
+        "runs": true
     }
 ];
 module.exports.metadata = 
@@ -45,7 +75,8 @@ module.exports.metadata =
     "cordova-plugin-whitelist": "1.3.3",
     "cordova-plugin-brightness": "0.1.5",
     "cordova-plugin-vibration": "3.1.0",
-    "cordova-plugin-battery-status": "2.0.2"
+    "cordova-plugin-battery-status": "2.0.2",
+    "cordova-plugin-camera": "4.0.3"
 }
 // BOTTOM OF METADATA
 });
